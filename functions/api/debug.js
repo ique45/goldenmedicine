39ee61ee22e1deb8e670a4d8f3c96c2ffdcd9f16
Ique Custodio <ique@tracker.com>
2026-05-09 16:04:54 -0300

    debug: check env vars at runtime

---
+++ functions/api/debug.js
@@ -0,0 +1,10 @@
+export async function onRequestGet(context) {
+  const { env } = context;
+  return new Response(JSON.stringify({
+    dashKeySet: !!env.DASH_KEY,
+    dashKeyLength: (env.DASH_KEY || '').length,
+    dbSet: !!env.DB,
+  }), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
